d19329aa7be1b21c894c05bc1ff5ce05ddaaf811
Yash Kumar Gandhir <yashk20030615@example.com>
2026-01-07 14:20:50 +0530

    Fix file name

--- frontend/src/Screen/signup.jsx
+++ frontend/src/Screen/signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./signup.css";
+import "./temp.css";
 
 const Signup = () => {
   const navigate = useNavigate();
